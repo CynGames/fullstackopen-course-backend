4abadc25e08c5e78e614a8018dd22b5fa4de24fc
CynGames <tomasm.leguizamon@gmail.com>
2022-04-22 13:20:59 -0300

    svbuild

--- models/person.js
+++ models/person.js
@@ -26,7 +26,7 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     validate: {
       validator: () => {
-        return /\d{3}-\d{3}-\d{4}/
+        return /\d{3}-\d{3}-\d{4}/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
